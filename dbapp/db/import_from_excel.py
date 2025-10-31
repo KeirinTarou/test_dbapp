4b720633c0a7d4dfc8cc35fbbbca10dae78eb59d
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-11-01 08:03:52 +0900

    [Add]'db/import_from_excel.py'Excel経由でDBに接続するテスト用コード追加

--- dbapp/db/import_from_excel.py
+++ dbapp/db/import_from_excel.py
@@ -2,19 +2,25 @@ import os
 import time
 import pandas as pd
 import win32com.client
+from dotenv import load_dotenv
+
+# `.env`読み込み
+load_dotenv()
+
+from connection import CONNECTION_STRING
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-excel_path = os.path.join(base_dir, "db_bridge.xlsm")
+excel_path = os.path.join(base_dir, "source.xlsm")
 
 excel = win32com.client.Dispatch("Excel.Application")
 excel.Visible = False
 wb = excel.Workbooks.Open(excel_path)
 
-sql = "SELECT TOP 10 * FROM Employees ORDER BY HireDate DESC"
-excel.Run("Module1.RefreshFromDB", sql)
+sql = "SELECT * FROM CustomerClasses;"
+excel.Run("Std01Main.RefreshFromDB", CONNECTION_STRING.replace("\n", ""), sql)
 
 # 完了待ち
-while wb.Names("RefreshFlag").RefersToRange.Value != "Done":
+while wb.Sheets("Status").Range("A1").Value != "Done!!":
     time.sleep(0.5)
 
 # 結果取得
