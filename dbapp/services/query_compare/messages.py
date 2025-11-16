77889c4f81df4d844dde96f2ec499dff3106a5c9
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-11-17 06:57:27 +0900

    [Update]'serveices/query_compare/messages.py': メッセージの内容を改訂

--- dbapp/services/query_compare/messages.py
+++ dbapp/services/query_compare/messages.py
@@ -14,12 +14,12 @@ class CompareResult(Enum):
 
 COMPARE_RESULT_MESSAGES = {
     CompareResult.OK: "🎉🎉🎉お見事！🎉🎉🎉",
-    CompareResult.COLUMN_COUNT_MISMATCH: "列数が異なっています。",
-    CompareResult.COLUMN_NAME_MISMATCH: "列名が異なっています。",
-    CompareResult.ROW_COUNT_MISMATCH: "行数が異なっています。",
-    CompareResult.ROW_ORDER_MISMATCH: "行の順序が異なっています。",
-    CompareResult.ROW_CONTENT_MISMATCH: "結果セットの内容が一致しません。",
-    CompareResult.OTHER_DIFFERENCE: "結果が一致しません。",
-    CompareResult.USER_HAS_EXTRA_ROWS: "ユーザークエリに余分な行があります。",
-    CompareResult.USER_MISSING_ROWS: "正解に存在する行がユーザークエリの結果にありません。",
+    CompareResult.COLUMN_COUNT_MISMATCH: "( ´,_ゝ`) < 列数が異なっています。",
+    CompareResult.COLUMN_NAME_MISMATCH: "( ´,_ゝ`) < 列名が異なっています。",
+    CompareResult.ROW_COUNT_MISMATCH: "( ´,_ゝ`) < 行数が異なっています。",
+    CompareResult.ROW_ORDER_MISMATCH: "( ´,_ゝ`) < 行の順序が異なっています。",
+    CompareResult.ROW_CONTENT_MISMATCH: "( ´,_ゝ`) < 結果セットの内容が一致しません。",
+    CompareResult.OTHER_DIFFERENCE: "( ´,_ゝ`) < 結果が一致しません。",
+    CompareResult.USER_HAS_EXTRA_ROWS: "( ´,_ゝ`) < ユーザークエリに余分な行があります。",
+    CompareResult.USER_MISSING_ROWS: "( ´,_ゝ`) < 正解に存在する行がユーザークエリの結果にありません。",
 }
