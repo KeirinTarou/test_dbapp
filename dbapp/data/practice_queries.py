9c4730d7f8d93e09e31c63e21c657f919198098d
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-11-29 15:35:51 +0900

    [Add]'data/practice_query.py': 問題データ取得・更新関数を追加

--- dbapp/data/practice_queries.py
+++ dbapp/data/practice_queries.py
@@ -89,6 +89,46 @@ EXISTS_QUESTION_QUERY = """
         AND q.QuestionNumber = ?
     ;
 """
+# 編集用に問題・正解のセットを取得するクエリ
+SELECT_QUESTION_DATA_QUERY = """
+    SELECT
+        q.ChapterNumber
+        , q.SectionNumber
+        , q.QuestionNumber
+        , q.Question
+        , q.AnswerQuery
+        , q.CheckMode
+        , c.ChapterTitle
+        , s.SectionTitle
+    FROM
+        Questions AS q
+        JOIN
+            Chapters AS c
+            ON c.ChapterNumber = q.ChapterNumber
+        JOIN
+            Sections AS s
+            ON s.SectionNumber = q.SectionNumber
+            AND s.ChapterNumber = q.ChapterNumber
+    WHERE
+        q.ChapterNumber = ?
+        AND q.SectionNumber = ?
+        AND q.QuestionNumber = ?
+    ;
+"""
+
+# 問題データ更新用クエリ
+UPDATE_QUESTION_QUERY = """
+    UPDATE Questions
+    SET
+        Question = ?
+        , AnswerQuery = ? 
+        , CheckMode = ?
+    WHERE
+        ChapterNumber = ?
+        AND SectionNumber = ?
+        AND QuestionNumber = ?
+    ;
+"""
 
 def fetch_all(sql_query: str, params: Optional[Sequence[Any]]=None) -> Tuple[List[str], List[Dict[str, Any]]]:
     """ 複数のレコードセットを取得する
@@ -117,3 +157,17 @@ def fetch_one(sql_query: str, params: Optional[Sequence[Any]]=None) -> Optional[
             return None
         # 取得した1件のレコードセットをDictにして返す
         return dict(zip(columns, row))
+
+def get_question_data(chapter_number: int, section_number: int, question_number):
+    return fetch_one(SELECT_QUESTION_DATA_QUERY, (chapter_number, section_number, question_number))
+
+def update_question(chapter_number:int, section_number: int, question_number: int, question_text: str, answer_query: str, check_mode: str = "strict"):
+    with get_connection() as conn:
+        cur = conn.cursor()
+        cur.execute(
+            UPDATE_QUESTION_QUERY, 
+            (question_text, answer_query, check_mode, chapter_number, section_number, question_number)
+        )
+        if cur.rowcount == 0:
+            raise ValueError("指定の問題が存在しません。")
+        conn.commit()
